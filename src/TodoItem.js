a71cc80b496a7b2430b4ae73111f9e81e39fc4eb
Jorge Diaz <jorjorf@gmail.com>
2023-06-24 19:15:34 -0400

    Componentes de TODO Machine

---
+++ src/TodoItem.js
@@ -0,0 +1,13 @@
+
+
+function TodoItem() {
+    return (
+      <li>
+        <span>V</span>
+        <p>Hola Mundo</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+export { TodoItem };
---
+++ src/TodoSearch.js
@@ -0,0 +1,8 @@
+
+function TodoSearch() {
+    return (
+      <input placeholder="Buscar"/>
+    );
+  }
+
+export { TodoSearch };
