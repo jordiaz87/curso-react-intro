0c3cf8d4d3ba9513db58a291a60f63d4428a3ac8
Jorge Diaz <jorjorf@gmail.com>
2023-07-05 13:21:29 -0400

    Iconos con colores dinámicos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,25 +6,18 @@ function TodoItem(props) {
     return (
       <li className='TodoItem'>
 
-        <CompleteIcon />
+        <CompleteIcon 
+          completed={props.completed}
+          onComplete={props.onComplete}
+        />
 
-        {/* <span 
-          className={`Icon Icon-check ${props.completed && "Icon-check--active"} `}
-          onClick={props.onComplete}
-        >
-          V 
-        </span> */}
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
         </p>
 
-        <DeleteIcon />
-        {/* <span 
-          className='Icon Icon-delete'
-          onClick={props.onDelete}
-        >
-        X
-        </span> */}
+        <DeleteIcon 
+          onDelete={props.onDelete}
+        />
       </li>
     );
   }
