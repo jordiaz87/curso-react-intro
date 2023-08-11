93d2a829fcb17d5cc0a78b60ac0140856000c8ee
Jorge Diaz <jorjorf@gmail.com>
2023-08-11 13:24:22 -0400

    Reto: loading skeletons

---
+++ src/TodosLoading/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TodosLoading.css';
+
+function TodosLoading() {
+    return (
+      // <div className='LoadingTodo-container'>
+      //   <span className='LoadingTodo-completeIcon'>
+      //       <p className='LoadingTodo-text'></p>
+      //   </span>
+      //   <span className='LoadingTodo-deleteIcon'>
+
+      //   </span>
+      // </div>
+
+      <div className="custom-loader"></div>
+      
+    );
+  }
+
+export { TodosLoading };
