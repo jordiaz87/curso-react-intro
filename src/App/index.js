380cd54834eab637336eddc507100872b2aff5c3
Jorge Diaz <jorjorf@gmail.com>
2023-08-11 16:08:52 -0400

    ¿Qué es React Context?

--- src/App/index.js
+++ src/App/index.js
@@ -1,69 +1,17 @@
 import React from 'react';
 import { AppUi } from './AppUi';
-import { useLocalStorage } from './useLocalStorage';
+import { TodoProvider } from '../TodoContext';
 
-// localStorage.removeItem('TODOS_V1');
 
-// const defaultTodos = [
-//   { text: 'Cortar cebolla', completed: true },
-//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
-//   { text: 'Llorar con la Llorona', completed: false },
-//   { text: 'LALALALALA', completed: false },
-//   { text: 'Usar estados derivados', completed: true },
-// ];
 
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 
 function App() {
-  const {
-    item: todos, 
-    saveItem: saveTodos,
-    loading,
-    error
-  } = useLocalStorage('TODOS_V1', []);
-  const [searchValue, setSearchValue ] = React.useState('');
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
- 
-
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLocaleLowerCase();
-      const searchText = searchValue.toLocaleLowerCase();
-      return todoText.includes(searchText);
-    }
-  )
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
+  
 
   return (
-    <AppUi 
-      loading={loading}
-      error={error}
-      completedTodos={completedTodos}
-      totalTodos={totalTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
+    <TodoProvider>
+        <AppUi />
+    </TodoProvider>
   );
 }
 
